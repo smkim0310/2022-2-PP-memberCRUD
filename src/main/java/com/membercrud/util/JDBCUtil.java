53ad76d09c160ad6eebb26a5df905338b888ad4d
smkim0310 <smkim0310mail@gmail.com>
2022-11-19 21:16:17 +0900

    JDBCUtil.java created

---
+++ src/main/java/com/membercrud/util/JDBCUtil.java
@@ -0,0 +1,25 @@
+package com.membercrud.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mysql://walab.handong.edu:3306/p2_21900112","p2_21900112","wu3eh7oTie");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
+
+    public static void main(String ars[]) {
+        Connection conn = getConnection();
+        if(conn != null)
+            System.out.println("DB 연결됨!");
+        else
+            System.out.println("DB 연결중 오류 !");
+    }
+}
